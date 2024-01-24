c300eb9fad7e803ddaff42199a2e2f94c2764428
Rahul Biswas <biswasrahul016@gmail.com>
2024-01-24 14:36:41 +0530

    Update functions.js

--- 520/functions.js
+++ 520/functions.js
@@ -55,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Mins <span class=\"digit\">" + minutes + "</span> Secs <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
